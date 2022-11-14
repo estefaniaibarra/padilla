f92b50d7c8c9a3597b3971b7f4c494e25a080246
191G0260@rcarbonifera.tecnm.mx <191G0260@rcarbonifera.tecnm.mx>
2022-11-14 14:44:52 -0600

    Agregar archivos de proyecto.

---
+++ Services/AlbumService.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace FotoAlbum.Services
+{
+    public class AlbumService
+    {
+        HttpListener server= new HttpListener();
+        public void Start()
+        {
+            if (!server.IsListening)
+            {
+                server.Prefixes.Add("http://*:9876/album/");
+                server.Start();
+                Escuchar();
+              
+            }
+        }
+        void Escuchar()
+        {
+            Thread peticion= new Thread(new ThreadStart(Atender));
+            peticion.IsBackground = true;
+            peticion.Start();
+        }
+        public event Action<string> ImagenRecibida;
+        void Atender()
+        {
+            var context = server.GetContext();
+            Escuchar();
+
+            //Atender
+            if (context.Request.Url.LocalPath == "/album/")
+            {
+                byte[] buffer = File.ReadAllBytes("Assets/HTMLPage1.html");
+                context.Response.ContentType = "text/html";
+                context.Response.OutputStream.Write(buffer, 0, buffer.Length);
+             
+                context.Response.StatusCode = 200;
+            }
+            else if (context.Request.Url.LocalPath=="/album/imagen" && context.Request.HttpMethod=="POST")
+            {
+                var stream = new StreamReader(context.Request.InputStream);
+                var data = HttpUtility.UrlDecode(stream.ReadToEnd());
+                //Tomar solo lo que este despues del MIME type
+
+                var imageb64 = data.Substring(data.IndexOf(',')+1);
+               byte[] buffer = Convert.FromBase64String(imageb64);
+                File.WriteAllBytes($"assets/imagen_{DateTime.Now.ToString("ddMMyyyyhhmmss")}.png",buffer);
+
+                string ruta = $"assets/imagen_{DateTime.Now.ToString("ddMMyyyyhhmmss")}.png";
+                File.WriteAllBytes(ruta, buffer);
+                ImagenRecibida?.Invoke(ruta);
+
+                context.Response.StatusCode = 200;//estado de proceso OK
+                context.Response.Redirect("/album/");//redirecciona a la pagina principal
+            }
+            else
+            {
+                context.Response.StatusCode = 404;
+            }
+            context.Response.Close();
+
+
+
+
+        }
+    }
+}
---
+++ ViewModels/AlbumViewModel.cs
@@ -0,0 +1,75 @@
+﻿using FotoAlbum.Services;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.IO;
+using System.Windows.Media.Imaging;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Threading;
+
+namespace FotoAlbum.ViewModels
+{
+    internal class AlbumViewModel:INotifyPropertyChanged
+    {
+        public ObservableCollection<BitmapImage> Imagenes { get; set; }= new ObservableCollection<BitmapImage>();
+        AlbumService server = new AlbumService();
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public BitmapImage? Seleccionada { get; set; }
+        int indice = 0;
+        DispatcherTimer timer = new DispatcherTimer();
+        Dispatcher principal= Dispatcher.CurrentDispatcher;
+        public ICommand IniciarCommand { get; set; }
+        public AlbumViewModel()
+        {
+            var ruta = Environment.CurrentDirectory;
+            var files = Directory.GetFiles("assets", "*.png");
+            
+            foreach (var item in files)
+            {
+                BitmapImage image = new BitmapImage(new Uri(ruta + "/" + item));
+                Imagenes.Add(image);
+
+            }
+            Seleccionada = Imagenes.FirstOrDefault();
+            timer.Interval = TimeSpan.FromSeconds(10);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+            server.ImagenRecibida += Server_ImagenRecibida;
+
+            IniciarCommand = new RelayCommand(() => server.Start());
+        }
+
+        private void Server_ImagenRecibida(string obj)
+        {
+            principal.Invoke(() =>
+            {
+                var ruta = Environment.CurrentDirectory;
+                BitmapImage image = new BitmapImage(new Uri(ruta + "/" + obj));
+                Imagenes.Add(image);
+
+                Seleccionada = image;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Seleccionada)));
+
+                timer.Stop();
+                timer.Start();
+            });
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            indice++;
+            if (indice==Imagenes.Count)
+            {
+                indice = 0;
+            }
+            Seleccionada = Imagenes.ElementAtOrDefault(indice);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Seleccionada)));
+        }
+    }
+}
